a0860a90a4c854b68d47cfc695b1341e62d57c12
CookieCat <81494827+CookieCat45@users.noreply.github.com>
2023-09-24 13:46:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const WSServer = require("ws").WebSocketServer;
 const prompt = require("prompt-sync")();
 const { Client, ClientPacket } = require("archipelago.js");
+const { EventEmitter } = require("ws");
 
 
 let client= new Client();
 
 async function main() {
     let initialRoomInfo;
+    let initialConnected;
     client.addListener("RoomInfo", packet => initialRoomInfo = packet);
-
+    client.addListener("Connected", packet => initialConnected = packet);
+    
     while (true) {
         try {
             const hostname = prompt("Enter the server hostname (e.g. 'archipelgo.gg'): ");
             const port = parseInt(prompt("Enter the server port (e.g. '38281'): "));
             const name = prompt("Enter your slot name: ");
             const password = prompt("Enter the server password (if any, otherwise leave blank): ");
-
+            
             await client.connect({
                 hostname,
                 port,
@@ -27,29 +30,36 @@ async function main() {
                     minor: 4,
                     build: 2,
                 },
-                game: "",
-                tags: ["TextOnly", "Proxy"],
+                game: "A Hat in Time",
+                tags: ["Proxy"],
                 items_handling: 7,
             });
-
+            
             break;
         } catch {
             console.error("Unable to connect... try again!\n");
         }
     }
-
+    
     const localServer = new WSServer({ port: 0 });
     const localServerPort = localServer.address().port;
-    console.log(`Connected to Archipelago server. Connect your client to 'ws://localhost:${localServerPort}'`);
-
+    console.log(`Connected to Archipelago server. Connect your client to 'localhost:${localServerPort}'`);
+    
     localServer.on("connection", (socket) => {
         console.log("Client connected...\n");
         socket.send(JSON.stringify([initialRoomInfo]));
-        client.addListener("PacketReceived", (packet) => {
+        
+        client.removeListener("PacketReceieved", onPacket);
+        client.addListener("PacketReceived", onPacket);
+        
+        function onPacket(packet) {
             socket.send(JSON.stringify([packet]));
-        });
-
-        client.addListener("PrintJSON", (packet, message) => {
+        }
+        
+        // client.removeListener doesn't work with PrintJSON for some reason, which leads to a memory leak + message spam
+        /*
+        function onPrint(packet, message)
+        {
             let string = "";
             if (packet.type === "Chat") {
                 string = `${client.players.alias(packet.slot)}: ${message}`;
@@ -58,18 +68,26 @@ async function main() {
             } else {
                 string = message;
             }
-
+            
             console.log(string);
-        });
-
+        }
+        */
+        
         socket.on("message", (message) => {
-            const packets = JSON.parse(message);
+            let packets = JSON.parse(message);
+            
             for (let i = 0; i < packets.length; i++) {
-                if ((packets[i].cmd === "Connect" || packets[i].cmd === "ConnectUpdate") && !packets.includes("Proxy")) {
+                if (packets[i].cmd == "Connect") {
+                    socket.send(JSON.stringify([initialConnected]));
+                } else if (packets[i].cmd === "ConnectUpdate" && !packets.includes("Proxy")) {
                     packets[i].tags.push("Proxy");
                 }
             }
-
+            
+            packets = packets.filter(function( packet ) {
+                return packet.cmd !== "Connect";
+            });
+            
             client.send(...packets);
         })
     });
